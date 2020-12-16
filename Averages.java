fec455d7edabd728fc2ba13a9b1d61c9c2345a00
22lduncan <73915056+22lduncan@users.noreply.github.com>
2020-12-16 16:32:41 -0500

    Add files via upload

---
+++ Averages.java
@@ -0,0 +1,24 @@
+/*22lduncan
+ * 12/16/20, 1:13 PM */
+
+import java.util.Scanner;
+public class Averages {
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scan.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scan.nextDouble();
+        System.out.print("Enter the third number: ");
+        double num3 = scan.nextDouble();
+        scan.close();
+        System.out.print("The average of entered numbers is:" + avr(num1, num2, num3) );
+    }
+
+    public static double avr(double a, double b, double c)
+    {
+        return (a + b + c) / 3;
+    }
+}
